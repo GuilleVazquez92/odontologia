ef5550da838221f4bc72c2e1d01eeb04d53e47d4
GuilleVazquez92 <guillevaz92@gmail.com>
2022-11-06 15:23:28 -0300

    cambio index calendario

--- resources/views/calendario/index.blade.php
+++ resources/views/calendario/index.blade.php
@@ -84,7 +84,7 @@
 
       },
 
-      events : '/calendario/mostrar', 
+      events : '/public/index.php/calendario/mostrar', 
 
       dateClick:function(info){
 
@@ -99,7 +99,7 @@
         var evento = info.event;
         console.log(evento.id);
 
-        axios.post('/calendario/editar/'+info.event.id).
+        axios.post('/public/index.php/calendario/editar/'+info.event.id).
         then(
           (response) => 
           { 
@@ -127,7 +127,7 @@
         const datos = new FormData(formulario);
         
 
-        axios.post('/calendario/agregar', datos).
+        axios.post('/public/index.php/calendario/agregar', datos).
         then(
           (response) => 
           { 
